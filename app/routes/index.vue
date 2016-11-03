bfbcefa39a88850e886c425928c682c4359ad63e
Derrick Bray <derrickbray@yahoo.com>
2016-11-03 16:46:34 -0500

    markup new vue

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,30 +1,34 @@
 <template lang="html">
   <div class="">
     <a class="button is-primary is-pulled-right">Add a Pupper</a>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Age</th>
-    </tr>
-    <tbody>
-      <tr v-for="item in lunchSpots">
-        <td>{{ puppy.name }}</td>
-        <td>{{ puppy.age }}</td>
-        <td>
-          <router-link :to="{ name: 'detail', params: {id:puppy.id} }">Read More</router-link>
-        </td>
-      </tr>
-    </tbody>
-  </thead>
-  </div>
+    <h1 class="title">Puppies for Adoption</h1>
+    <table  class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="puppy.adopted" v-for="puppy in puppies">
+          <td>{{ puppy.name }}</td>
+          <td>{{ puppy.age }}</td>
+          <td>
+            <router-link :to="{ name: 'detail', params: {id:puppy.id} }">Read More</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</div>
 </template>
 
 <script>
 export default {
+  props: ['puppies'],
+
   data() {
-    return {
-    };
+    return {};
   },
 
   methods: {
